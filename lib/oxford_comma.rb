493a647af28f52dda260933c50abb1ceac49a748
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 14:21:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
+  if array.length == 2
     array.join(" and ")
   else 
     array[-1] = "and " + array[-1]
     array.join(", ")
   end
+  array.join(" ,")
 end
