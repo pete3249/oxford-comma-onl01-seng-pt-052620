140979a0597341c50381e27bc872224095c10daa
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-02 14:10:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array.each do |item|
+      if item == array[-1]
+        array[-1] = "and " + item
+      end
+    end 
+    array.join(", ")
+  else
+    array.each do |item|
+      if item == array[-1]
+        array[-1] = "and " + item
+      end
+    end
+    array.join(", ")
+  end
 end
